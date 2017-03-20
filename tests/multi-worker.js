e1dfd766ada352addfd5b0c8e3e71769d895a56b
YanagiEiichi <admin@web-tinker.com>
2017-03-20 18:05:44 +0800

    Catch SOCKET_IPC_PID_NOT_FOUND

--- tests/multi-worker.js
+++ tests/multi-worker.js
@@ -1,6 +1,11 @@
 const cluster = require('cluster');
-const IPC = require('..');
+const { broadcast, registerMaster, register, call } = require('..');
 const SequenceTester = require('sequence-tester');
+
+const die = (error) => {
+  console.error(error);
+  process.exit(1);
+};
 
 if (cluster.isMaster) {
 
@@ -9,34 +14,29 @@ if (cluster.isMaster) {
   const allWorkerReady = new SequenceTester(Array.from({ length: TEST_COUNT }, () => true));
   const allWorkerExited = new SequenceTester(Array.from({ length: TEST_COUNT }, () => true));
 
-  const die = (error) => {
-    console.error(error);
-    process.exit();
-  };
-
   allWorkerReady.then(() => {
-    IPC.broadcast('exit');
+    broadcast('exit').catch(() => { /* ignore error */ });
   }).catch(die);
 
   allWorkerExited.then(() => {
     process.exit(0);
   }).catch(die);
 
-  IPC.registerMaster('workerExited', () => allWorkerExited.assert(true));
+  registerMaster('workerExited', () => allWorkerExited.assert(true));
 
-  IPC.registerMaster('workerReady', () => allWorkerReady.assert(true));
+  registerMaster('workerReady', () => allWorkerReady.assert(true));
 
   for (let i = 0; i < TEST_COUNT; i++) cluster.fork();
 
 } else {
 
-  IPC.register({
+  register({
     exit() {
-      IPC.call('workerExited');
+      call('workerExited').catch(die);
       process.nextTick(() => process.exit(0));
     }
   });
 
-  IPC.call('workerReady');
+  call('workerReady').catch(die);
 
 }
