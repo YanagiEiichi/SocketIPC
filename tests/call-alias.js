8c66ac2e91ceb9acbe1c4ae635057818d255e461
王金海 <jinhai.wang@ele.me>
2017-04-25 17:15:30 +0800

    Linter test.

--- tests/call-alias.js
+++ tests/call-alias.js
@@ -1,43 +1,41 @@
 const cluster = require('cluster');
 const assert = require('assert');
 const SequenceTester = require('sequence-tester');
-const { call, register, registerMaster, broadcast, callWorker } = require('..');
-const { setAlias, callAlias, ready } = require('..');
+const { call, register, registerMaster, setAlias, callAlias, ready } = require('..');
 
-const workerAlias = 'worker'
+const workerAlias = 'worker';
 let COUNT = 3;
 
 if (cluster.isMaster) {
   try {
-    setAlias('test')
+    setAlias('test');
   } catch (e) {
-    assert(e.name, 'SOCKET_IPC_PID_NOT_FOUND')
+    assert(e.name, 'SOCKET_IPC_PID_NOT_FOUND');
   }
 
   callAlias(workerAlias, 'ok').catch(err => {
-    assert(err.name, 'SOCKET_IPC_ALIAS_NOT_FOUND_ERROR')
-  })
+    assert(err.name, 'SOCKET_IPC_ALIAS_NOT_FOUND_ERROR');
+  });
 
   ready().then(() => {
     setAlias('master').then(result => assert.equal(result, true));
 
     try {
-      setAlias('error')
+      setAlias('error');
     } catch (e) {
-      assert(e.name, 'SOCKET_IPC_ALREADY_ALIAS_ERROR')
+      assert(e.name, 'SOCKET_IPC_ALREADY_ALIAS_ERROR');
     }
 
     let stInit = new SequenceTester(Array.from({ length: COUNT }, () => true));
 
     registerMaster('init', () => stInit.assert(true));
 
-
     stInit.then(() => {
       return callAlias(workerAlias, 'ok');
     }).then((result) => {
       assert.deepEqual(result, Array.from({ length: COUNT }, () => 'ok'));
     }).then(() => {
-      return new Promise((resolve, reject) => {
+      return new Promise((resolve) => {
         workers[0].kill();
         workers[0].on('exit', () => {
           resolve();
@@ -56,15 +54,13 @@ if (cluster.isMaster) {
   }
 } else {
   register('ok', () => {
-    return Promise.resolve('ok')
+    return Promise.resolve('ok');
   });
-
-  let st2 = new SequenceTester(Array.from({ length: COUNT }, () => true));
 
   ready().then(() => {
     setAlias('worker').then(results => {
       call('init');
       assert.equal(results, true);
     });
-  })
+  });
 }
